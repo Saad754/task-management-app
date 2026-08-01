1332741bf5411f0c13db4ff5242bac05dc2261a6
Saad ElSokkary <saad.777555@gmail.com>
2026-08-01 17:12:28 +0300

    Added test profile to application context test

--- backend/src/test/java/com/oie/taskmanagement/TaskManagementAppApplicationTests.java
+++ backend/src/test/java/com/oie/taskmanagement/TaskManagementAppApplicationTests.java
@@ -2,12 +2,13 @@ package com.oie.taskmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TaskManagementAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
